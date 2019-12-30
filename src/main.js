ba53a822a9de451c6eb97c52c3acae06c0aad01f
Børge S. Dreyer <borge.dreyer@gmail.com>
2019-12-30 18:52:26 +0100

    add scaling for mobile

--- src/main.js
+++ src/main.js
@@ -1,120 +1,122 @@
 let working_experience = [
-    {
-        name: "MENY",
-        title: "Butikkmedarbeider",
-        place: "Drøbak",
-        period: "Høst 2014 - høst 2017",
-        logo: "img/meny.jpg",
-        alt: "meny logo",
-        descreption: "Jobbet som lager- og kassemedarbeider i 3 år mens jeg gikk på videregående. Hadde stengeansvar og varierte arbeidsoppgaver rundt i butikken.",
-        priority: 80
-    },
-    {
-        name: "Nordisk Fiskeutstyr",
-        title: "Salgsmedarbeider",
-        place: "Lysaker",
-        period: "Sommer 2018",
-        logo: "img/nfisk.jpg",
-        alt: "nfisk logo",
-        descreption: "Var sesongansatt og drev stort sett med salg av fiskeutstyr og var til hjelp for kunder i butikk, over telefon og e-post.",
-        priority: 70
-    },
-    {
-        name: "Dotkom",
-        title: "Komiteemedlem",
-        place: "Trondheim",
-        period: "Høst 2019 - d.d",
-        logo: "img/online.png",
-        alt: "online-ikon logo",
-        descreption: "Drift- og utviklingskomiteen (dotkom) er ansvarlig for nettsiden og driften rundt systemene til online.ntnu.no. Som komitemedlem/utvikler i dotkom er man med på å forbedre og utvikle tjenestene på weben. Teknologiene som blir brukt nå er for det meste Django, men vi jobber med en ny versjon av siden i React. For tiden jobber jeg på et komponent-bibliotek i React/typescript. ",
-        priority: 100
-    },
+  {
+    name: "MENY",
+    title: "Butikkmedarbeider",
+    place: "Drøbak",
+    period: "Høst 2014 - høst 2017",
+    logo: "img/meny.jpg",
+    alt: "meny logo",
+    descreption:
+      "Jobbet som lager- og kassemedarbeider i 3 år mens jeg gikk på videregående. Hadde stengeansvar og varierte arbeidsoppgaver rundt i butikken.",
+    priority: 80
+  },
+  {
+    name: "Nordisk Fiskeutstyr",
+    title: "Salgsmedarbeider",
+    place: "Lysaker",
+    period: "Sommer 2018",
+    logo: "img/nfisk.jpg",
+    alt: "nfisk logo",
+    descreption:
+      "Var sesongansatt og drev stort sett med salg av fiskeutstyr og var til hjelp for kunder i butikk, over telefon og e-post.",
+    priority: 70
+  },
+  {
+    name: "Dotkom",
+    title: "Komiteemedlem",
+    place: "Trondheim",
+    period: "Høst 2019 - d.d",
+    logo: "img/online.png",
+    alt: "online-ikon logo",
+    descreption:
+      "Drift- og utviklingskomiteen (dotkom) er ansvarlig for nettsiden og driften rundt systemene til online.ntnu.no. Som komitemedlem/utvikler i dotkom er man med på å forbedre og utvikle tjenestene på weben. Teknologiene som blir brukt nå er for det meste Django, men vi jobber med en ny versjon av siden i React. For tiden jobber jeg på et komponent-bibliotek i React/typescript. ",
+    priority: 100
+  }
 ];
 
-const compare = (a,b) => (a.priority > b.priority ? -1 : b.priority > a.priority ? 1 : 0);
-const sortedJobs = working_experience.sort(compare)
+const compare = (a, b) =>
+  a.priority > b.priority ? -1 : b.priority > a.priority ? 1 : 0;
+const sortedJobs = working_experience.sort(compare);
 sortedJobs.length = 3; //How many jobs that are showing
 
-function renderArray(array,place) {
+function renderArray(array, place) {
+  array.forEach(element => {
+    let li = document.createElement("li");
+    li.className = "info-li";
 
-    array.forEach(element => {
-        let li = document.createElement("li");
-        li.className = "info-li";
+    let container = document.createElement("section");
+    container.className = "content-container";
 
-        let container = document.createElement("section");
-        container.className = "content-container";
-
-        let header = document.createElement("header");
-        header.className = "header-container";
-        let h2 = document.createElement("h2");
-        h2.className = "header-text";
-        h2.textContent = `${element.title}
+    let header = document.createElement("header");
+    header.className = "header-container";
+    let h2 = document.createElement("h2");
+    h2.className = "header-text";
+    h2.textContent = `${element.title}
         ${element.name}
-        `;
-        /*
+        `; //uncomment when pictures are added to img. //header.appendChild(img)
+    /*
         let img = document.createElement("img");
         img.className = "job-logo";
-        img.src = element.logo;*/ //uncomment when pictures are added to img. 
+        img.src = element.logo;*/ header.appendChild(
+      h2
+    );
 
-        //header.appendChild(img)
-        header.appendChild(h2)
+    let from_to = document.createElement("p");
+    from_to.className = "period-p";
+    from_to.textContent = element.period;
 
-        let from_to = document.createElement("p");
-        from_to.className = "period-p";
-        from_to.textContent = element.period;
+    let info_descreption = document.createElement("p");
+    info_descreption.className = "info-descreption";
+    info_descreption.textContent = element.descreption;
 
-        let info_descreption = document.createElement("p");
-        info_descreption.className = "info-descreption";
-        info_descreption.textContent = element.descreption;
-
-        container.appendChild(header)
-        container.appendChild(from_to)
-        container.appendChild(info_descreption)
-        li.appendChild(container)
-        place.appendChild(li)
-    });
+    container.appendChild(header);
+    container.appendChild(from_to);
+    container.appendChild(info_descreption);
+    li.appendChild(container);
+    place.appendChild(li);
+  });
 }
 const job_ul = document.getElementById("experience-list");
-renderArray(sortedJobs,job_ul)
+renderArray(sortedJobs, job_ul);
 
 const education = [
-    {
-        title: "Studiespesialisering",
-        name: "frogn VGS",
-        place: "Drøbak",
-        period: "2014 - 2017",
-        logo: "img/frogn.png",
-        alt: "frogn logo",
-        descreption: "Studiespesialisering med realfag",
-    },
-    {
-        title: "Matteknologi",
-        name: "NTNU",
-        place: "Trondheim",
-        period: "2018 - 2019",
-        logo: "img/NTNU.png",
-        alt: "NTNU logo",
-        descreption: "Gikk ett år på bachelor matteknologi",
-    },
-    {
-        title: "Informatikk",
-        name: "NTNU",
-        place: "Trondheim",
-        period: "2019 - d.d",
-        logo: "img/NTNU.png",
-        alt: "NTNU logo",
-        descreption: "3-åring bachelor",
-    },
-    {
-        title: "Sportsfiske",
-        name: "Sund folkehøgskole",
-        place: "Inderøy",
-        period: "2017 - 2018",
-        logo: "img/sund.png",
-        alt: "Sund logo",
-        descreption: "Ett år på folkehøgskole på linjen sportsfiske."   
-    }
+  {
+    title: "Studiespesialisering",
+    name: "frogn VGS",
+    place: "Drøbak",
+    period: "2014 - 2017",
+    logo: "img/frogn.png",
+    alt: "frogn logo",
+    descreption: "Studiespesialisering med realfag"
+  },
+  {
+    title: "Matteknologi",
+    name: "NTNU",
+    place: "Trondheim",
+    period: "2018 - 2019",
+    logo: "img/NTNU.png",
+    alt: "NTNU logo",
+    descreption: "Gikk ett år på bachelor matteknologi"
+  },
+  {
+    title: "Informatikk",
+    name: "NTNU",
+    place: "Trondheim",
+    period: "2019 - d.d",
+    logo: "img/NTNU.png",
+    alt: "NTNU logo",
+    descreption: "3-åring bachelor"
+  },
+  {
+    title: "Sportsfiske",
+    name: "Sund folkehøgskole",
+    place: "Inderøy",
+    period: "2017 - 2018",
+    logo: "img/sund.png",
+    alt: "Sund logo",
+    descreption: "Ett år på folkehøgskole på linjen sportsfiske."
+  }
 ];
 
-const school_ul = document.getElementById("school-ul")
-renderArray(education,school_ul)
+const school_ul = document.getElementById("school-ul");
+renderArray(education, school_ul);
